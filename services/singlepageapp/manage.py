7de4756e90a51805d5c5981f849a6c9d55d1ce96
drogonj <drogonjm@gmail.com>
2024-05-29 16:56:09 +0200

    Dev-(SPA)-Working with staticfiles todo: - Authentification adapation

--- services/singlepageapp/manage.py
+++ services/singlepageapp/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asgi.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'singlepageapp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
