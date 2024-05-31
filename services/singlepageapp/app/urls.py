6ec05876d8a75e29d7a88f3c702750e176ca9714
drogonj <drogonjm@gmail.com>
2024-05-31 14:44:42 +0200

    Dev-(Django/Nginx)-Managements

--- services/singlepageapp/app/urls.py
+++ services/singlepageapp/app/urls.py
@@ -5,12 +5,11 @@ from django.views.generic.base import RedirectView
 from .views import LoginView, SignupView, LogoutView, IsAuthenticatedView, IndexView, get_csrf_token
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
     path('api/login/', LoginView.as_view(), name='login'),
     path('api/logout/', LogoutView.as_view(), name='logout'),
     path('api/signup/', SignupView.as_view(), name='signup'),
     path('api/is_authenticated/', IsAuthenticatedView.as_view(), name='is_authenticated'),
     path('api/get_csrf_token/', get_csrf_token, name='get_csrf_token'),
     re_path(r'^.*\..*$', TemplateView.as_view(template_name="404.html"), name='file_not_found'),
-    re_path(r'^.*$', RedirectView.as_view(url='/', permanent=False), name='redirect'),
+    re_path(r'^.*$', IndexView.as_view(), name='index'),
 ]
---
+++ services/singlepageapp/static/js/auth.js
@@ -0,0 +1,76 @@
+import { navigateTo } from "./contentLoader.js";
+
+export let csrfToken = '';
+
+async function getCsrfToken() {
+    const response = await fetch('/api/get_csrf_token/');
+    const data = await response.json();
+    csrfToken = data.csrfToken;
+}
+
+export async function handleLogin(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    await getCsrfToken();
+
+    const response = await fetch('/api/login/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrfToken
+        },
+        body: JSON.stringify({ username: username, password: password })
+    });
+    const data = await response.json();
+    if (data.success) {
+        navigateTo('/');
+    } else {
+        alert('Login failed: ' + data.message);
+    }
+}
+
+export async function handleSignup(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const signupData = {
+        username: formData.get('username'),
+        password: formData.get('password')
+    };
+
+    await getCsrfToken();
+
+    const response = await fetch('/api/signup/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrfToken
+        },
+        body: JSON.stringify(signupData)
+    });
+    const data = await response.json();
+    if (data.message) {
+        navigateTo('/login');
+    } else {
+        alert(data.error);
+    }
+}
+
+export async function handleLogout() {
+    await getCsrfToken();
+
+    const response = await fetch('/api/logout/', {
+        method: 'POST',
+        headers: {
+            'X-CSRFToken': csrfToken
+        }
+    });
+    const data = await response.json();
+    if (data.success) {
+        navigateTo('/login');
+    } else {
+        alert('Logout failed: ' + data.message);
+    }
+}
