38b1d25ae0fe931c0725b3ede8325114207dd143
chabrune <charlesbrunet51@gmail.com>
2024-06-26 15:12:21 +0200

    OWASP Rules added

--- services/front-end/entrypoint.sh
+++ services/front-end/entrypoint.sh
@@ -6,7 +6,6 @@ do
   sleep 1
 done
 
-echo 'SecRule ARGS:testparam "@contains test" "id:1234,deny,status:403' >> etc/nginx/modsec/modsecurity.conf
 cp -r /tmp_static/* /singlepageapp
 
 nginx -g 'daemon off;'
