8b86d677305a44b942d27fa10fe1d5535a583048
AliHaine <alitr57120@gmail.com>
2024-07-01 04:35:36 +0200

    Update the paddle to be totally responsive, update header and fix minor bug in game loop

--- services/front-end/src/view/player_view.js
+++ services/front-end/src/view/player_view.js
@@ -1,11 +1,11 @@
 import {moveStep} from "../controller/settings.js";
 
 export function movePlayerPaddleUp(playerPaddle) {
-	playerPaddle.paddleHtml.style.top = (playerPaddle.paddleHtml.offsetTop - moveStep) + "px";
+	playerPaddle.paddleHtml.style.top = --playerPaddle.playerTopPosition + "%";
 }
 
 export function movePlayerPaddleDown(playerPaddle) {
-	playerPaddle.paddleHtml.style.top = (playerPaddle.paddleHtml.offsetTop + moveStep) + "px";
+	playerPaddle.paddleHtml.style.top = ++playerPaddle.playerTopPosition + "%";
 }
 
 export function displayPlayerPoint(scoreHtml, text) {
