92fd49c6c9df40d0045360cf92a3627169ace325
jmathieu <jmathieu@student.42mulhouse.fr>
2024-07-30 17:31:30 +0200

    loading js

--- services/front-end/scripts/chat.js
+++ services/front-end/scripts/chat.js
@@ -1,65 +1,7 @@
 let chatSocket = null;
 let chatSocketRunning = false;
-let userSocket = null;
-let userSocketRunning = false;
 
 import { navigateTo, app } from './contentLoader.js';
-
-// export async function connectUserWebsocket() {
-// 	userSocket = new WebSocket('wss://localhost:8080/ws/chat/users/');
-
-// 	userSocket.onopen = function(e) {
-// 		chatSocketRunning = true;
-// 		console.log("User connection established.");
-// 	};
-
-// 	userSocket.onmessage = function(e) {
-// 		const data = JSON.parse(e.data);
-// 		const type = data.type;
-// 		console.log(data);
-
-// 		const usersContainer = document.getElementById('users-content');
-// 		const user = document.getElementById(`user-${data.user_id}`);
-
-// 		const route =  window.location.pathname + window.location.search;
-//		 if (route === '/home' || route === '/home/') {
-//			 if (type === 'friend_request_notification') {
-//				 const avatar = data.avatar
-//				 addFriendshipRequestToMenu(user_id, from_user, avatar)
-//			 } else if (type === 'accepted_friendship_request_notification') {
-//				 const avatar = data.avatar
-//				 const is_connected = data.is_connected
-//				 addFriendToMenu(user_id, from_user, avatar, is_connected)
-//			 } else if (type === 'canceled_friendship_notification') {
-//				 const divId = "friend-" + user_id;
-//				 const element = document.getElementById(divId);
-//				 element.remove();
-//			 } else if (type === 'friend_connected_notification') {
-//				 changeFriendStatus(user_id, true);
-//			 } else if (type === 'friend_disconnected_notification') {
-//				 changeFriendStatus(user_id, false)
-//			 }
-// 		}
-
-// 		newMessage.classList.add('chat-message');
-// 		newMessage.textContent = data.message;
-// 		chatMessages.appendChild(newMessage);
-// 		chatMessages.scrollTop = chatMessages.scrollHeight;
-// 	};
-
-// 	userSocket.onclose = function(e) {
-// 		chatSocketRunning = false;
-// 		if (e.wasClean) {
-// 			console.log(`[close] User Connection closed cleanly, code=${e.code} reason=${e.reason}`);
-// 		} else {
-// 			console.log('[close] User Connection died');
-// 		}
-// 	};
-
-// 	userSocket.onerror = function(error) {
-// 		console.error(`[error] ${error.message}`);
-// 	};
-// }
 
 export async function connectChatWebsocket() {
 	const roomName = 'general';
