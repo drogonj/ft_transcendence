c61e33525c34e78c0900775cdbb72a732bcf0a03
chabrune <charlesbrunet51@gmail.com>
2024-09-09 14:50:10 +0200

    Testing with docker compose down

--- services/user-management/start.sh
+++ services/user-management/start.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-echo "Waiting for Vault 2 to be ready..."
+#!/bin/sh
+
+echo "Waiting for Vault 3 to be ready..."
 while true; do
-if curl -fs -o /dev/null --cacert /vault/ssl/ca.crt https://vault_2:8200/v1/sys/health; then
-    echo "Vault 2 is ready. Proceeding with startup."
+  echo "Attempting to connect to Vault 3..."
+  if curl -v -fs --cacert /vault/ssl/ca.crt https://vault_3:8200/v1/sys/health; then
+    echo "Vault 3 is ready. Proceeding with startup."
     break
-fi
-echo "Vault 2 is not ready yet. Retrying in 10 seconds..."
-sleep 10
+  else
+    echo "Curl exit code: $?"
+    echo "Vault 3 is not ready yet. Retrying in 20 seconds..."
+    sleep 20
+  fi
 done
+
 
 
 echo "----- Collect static files ------ " 
