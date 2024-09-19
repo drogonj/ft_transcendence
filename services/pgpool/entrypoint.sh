538a4c87cddb16fae2a99f0835cc722df7af52fb
chabrune <charlesbrunet51@gmail.com>
2024-09-20 00:45:55 +0200

    Little fix

--- services/pgpool/entrypoint.sh
+++ services/pgpool/entrypoint.sh
@@ -11,6 +11,7 @@ while true; do
   sleep 10
 done
 
+sleep 20
 SECRETS=$(curl -s -H "X-Vault-Token: $VAULT_TOKEN" --cacert /vault/ssl/ca.crt https://vault_2:8200/v1/secret/data/ft_transcendence/database | jq -r '.data.data')
 
 export PGPOOL_POSTGRES_USERNAME=$(echo $SECRETS | jq -r '.POSTGRESQL_USERNAME')
@@ -23,11 +24,16 @@ export PGPOOL_ADMIN_PASSWORD=$(echo $SECRETS | jq -r '.PGPOOL_ADMIN_PASSWORD')
 export PGPOOL_BACKEND_NODES="0:pg-0:5432,1:pg-1:5432"
 
 echo "${PGPOOL_POSTGRES_USERNAME}:$(pg_md5 ${PGPOOL_POSTGRES_PASSWORD})" > /opt/bitnami/pgpool/etc/pool_passwd
-#A VOIR POUR REMETTRE LE FICHIER DE CONFIGURATION DE PGPOOL 
-sed -i "s/^sr_check_user.*/sr_check_user = '${PGPOOL_SR_CHECK_USER}'/" /opt/bitnami/pgpool/etc/transcendence_pgpool.conf
-sed -i "s/^sr_check_password.*/sr_check_password = '${PGPOOL_SR_CHECK_PASSWORD}'/" /opt/bitnami/pgpool/etc/transcendence_pgpool.conf
-sed -i "s/^health_check_user.*/health_check_user = '${PGPOOL_POSTGRES_USERNAME}'/" /opt/bitnami/pgpool/etc/transcendence_pgpool.conf
-sed -i "s/^health_check_password.*/health_check_password = '${PGPOOL_POSTGRES_PASSWORD}'/" /opt/bitnami/pgpool/etc/transcendence_pgpool.conf
+
+#STILL HAVE AN ERROR (?) :
+# pgpool                 | 2024-09-19 22:40:37.339: sr_check_worker pid 274: ERROR:  Failed to check replication time lag
+# pgpool                 | 2024-09-19 22:40:37.339: sr_check_worker pid 274: DETAIL:  No persistent db connection for the node 1
+# pgpool                 | 2024-09-19 22:40:37.339: sr_check_worker pid 274: HINT:  check sr_check_user and sr_check_password
+# pgpool                 | 2024-09-19 22:40:37.339: sr_check_worker pid 274: CONTEXT:  while checking replication time lag
+sed -i "s/^sr_check_user.*/sr_check_user = '${PGPOOL_SR_CHECK_USER}'/" /opt/bitnami/pgpool/conf/transcendence_pgpool.conf
+sed -i "s/^sr_check_password.*/sr_check_password = '${PGPOOL_SR_CHECK_PASSWORD}'/" /opt/bitnami/pgpool/conf/transcendence_pgpool.conf
+sed -i "s/^health_check_user.*/health_check_user = '${PGPOOL_POSTGRES_USERNAME}'/" /opt/bitnami/pgpool/conf/transcendence_pgpool.conf
+sed -i "s/^health_check_password.*/health_check_password = '${PGPOOL_POSTGRES_PASSWORD}'/" /opt/bitnami/pgpool/conf/transcendence_pgpool.conf
 
 /opt/bitnami/scripts/pgpool/setup.sh
 
